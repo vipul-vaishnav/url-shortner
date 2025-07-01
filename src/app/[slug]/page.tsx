5d3b97014177aaabec43add68ef1f1a4c499920c
Vipul <007vipulrank1@gmail.com>
2025-07-01 17:06:54 +0530

    REM:await in params

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { urlRouterCaller } from '@/server/routers/url-router'
 import { notFound, redirect } from 'next/navigation'
 
 export default async function Slug({ params }: { params: { slug: string } }) {
-  const { slug } = await params
+  const { slug } = params
 
   const { destinationUrl } = await urlRouterCaller.getUrlBySlug(slug)
 
