f345765733a0aa66162983bdbd1db13452fe2ada
Vipul <007vipulrank1@gmail.com>
2025-07-01 17:01:58 +0530

    REM: component

--- src/components/CustomButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from './ui/button'
-
-type CustomButtonProps = {}
-
-const CustomButton: React.FC<CustomButtonProps> = () => {
-  return <Button>Have a good coding</Button>
-}
-export default CustomButton
