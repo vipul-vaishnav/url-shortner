408086f30a74a2048a8bc8fc5854dbf5f75cd909
Vipul <007vipulrank1@gmail.com>
2025-07-03 00:21:57 +0530

    FIX: error

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,10 +2,10 @@ import { urlRouterCaller } from '@/server/routers/url-router'
 import { TRPCError } from '@trpc/server'
 import { notFound, redirect } from 'next/navigation'
 
-export default async function Slug({ params }: { params: { slug: string } }) {
+export default async function Slug({ params }: { params: Promise<{ slug: string }> }) {
   let destinationUrlVal: string
   try {
-    const { slug } = params
+    const { slug } = await params
     const { destinationUrl } = await urlRouterCaller.getUrlBySlug(slug)
 
     destinationUrlVal = destinationUrl
